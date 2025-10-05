7dcb10115d3e80fc6747775bac9b8b197077c720
Simon Kwabla Azigie <simonziggy18@gmail.com>
2025-10-05 16:11:11 +0000

    bank account

--- programming_paradigm/bank_account.py
+++ programming_paradigm/bank_account.py
@@ -12,4 +12,4 @@ class BankAccount:
         else:
             return False
     def display_balance(self):
-        print (self.account_balance)
+        print (f"Current Balance: ${self.account_balance}")

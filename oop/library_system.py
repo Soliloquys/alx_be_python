a7a7d517a315647e4e2e45501af7245db21bd4da
Simon Kwabla Azigie <simonziggy18@gmail.com>
2025-10-12 10:04:05 +0000

    book

--- oop/library_system.py
+++ oop/library_system.py
@@ -11,7 +11,7 @@ class EBook(Book):
         super().__init__(title, author)
         self.file_size = int(file_size)
     def __str__(self):
-        return f"EBook: {self.title} by {self.author}"
+        return f"EBook: {self.title} by {self.author}, File Size: {self.file_size}"
 
 class PrintBook(Book):
     def __init__(self, title, author, page_count):
@@ -19,7 +19,7 @@ class PrintBook(Book):
         self.page_count = int(page_count)   
         
     def __str__(self):
-        return f"PrintBook: {self.title} by {self.author}" 
+        return f"PrintBook: {self.title} by {self.author}, Page Count: {self.page_count}" 
 
 class Library:
     def __init__(self):
